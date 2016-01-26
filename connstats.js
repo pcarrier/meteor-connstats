bd2b32aeb8411e9961fba750eb0e2431f6b754d3
Pierre Carrier <pierre@meteor.com>
2016-01-25 21:10:44 -0800

    Use ring arrays
    Optimization for write performance.

--- connstats.js
+++ connstats.js
@@ -1,3 +1,37 @@
+class RingArray {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this._entries = [];
+    this._position = 0;
+    this.available = 0;
+    for (let i = 0; i < capacity; i++) {
+      this._entries[i] = undefined;
+    }
+  }
+
+  push(entry) {
+    this._entries[this._position] = entry;
+    this._position = (this._position + 1) % this.capacity;
+    if (this.available < this.capacity) {
+      this.available += 1;
+    }
+    return entry;
+  }
+
+  entries(n) {
+    if (typeof n === 'undefined' || n >= this.available) {
+      n = this.available;
+    }
+    const result = [];
+    let pos = (this._position + this.capacity - n) % this.capacity;
+    for (let i = 0; i < n; i++) {
+      result[i] = this._entries[pos];
+      pos = (pos + 1) % this.capacity;
+    }
+    return result;
+  }
+}
+
 const URL = Npm.require('url'),
   defaultConfig = {
     hitSamples: 1024,
@@ -6,16 +40,10 @@ const URL = Npm.require('url'),
   },
   config = _.extend(defaultConfig, Meteor.settings.connstats),
   hitSamples = _.range(config.hitSamples).map(() => undefined),
-  openedSessions = [],
-  closedSessions = [],
+  openedSessions = new RingArray(config.openedSessions),
+  closedSessions = new RingArray(config.openedSessions),
   randBucket = () => _.random(config.hitSamples - 1),
   activeSessions = {},
-  append = (dest, entry, limit) => {
-    dest.push(entry);
-    if (dest.length > limit) {
-      dest.shift();
-    }
-  },
   connDescr = (conn) => ({
     ua: conn.httpHeaders['user-agent'],
     ip: conn.httpHeaders['x-forwarded-for'],
@@ -37,8 +65,8 @@ WebApp.connectHandlers.use((req, res, next) => {
     res.end(JSON.stringify({
       hitSamples: _.without(hitSamples, undefined),
       activeSessions: _.values(activeSessions),
-      openedSessions,
-      closedSessions,
+      openedSessions: openedSessions.entries(),
+      closedSessions: closedSessions.entries(),
     }));
   } else {
     hitSamples[randBucket()] = {
@@ -52,10 +80,10 @@ WebApp.connectHandlers.use((req, res, next) => {
 Meteor.onConnection(function(conn) {
   const descr = connDescr(conn);
   activeSessions[conn.id] = descr;
-  append(openedSessions, descr, config.openedSessions);
+  openedSessions.push(descr);
   conn.onClose(function() {
     delete activeSessions[conn.id];
     descr.stopped = new Date();
-    append(closedSessions, descr, config.closedSessions);
+    closedSessions.push(descr);
   });
 });
